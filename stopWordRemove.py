71b3b393175c04540fdd527b16e7b434d107f71e
Sanjeev1818 <67723640+Sanjeev1818@users.noreply.github.com>
2024-04-10 00:53:59 -0400

    Add files via upload

---
+++ stopWordRemove.py
@@ -0,0 +1,14 @@
+#import nltk
+#nltk.download('stopwords')
+#nltk.download('punkt')
+# nltk.download('wordnet')
+
+# nltk.download('pros_cons')
+# nltk.download('reuters')
+import nltk
+# Perform other imports or code execution here
+nltk.download('stopwords', quiet=True, raise_on_error=False)
+nltk.download('punkt', quiet=True, raise_on_error=False)
+nltk.download('wordnet', quiet=True, raise_on_error=False)
+nltk.download('pros_cons', quiet=True, raise_on_error=False)
+nltk.download('reuters', quiet=True, raise_on_error=False)
